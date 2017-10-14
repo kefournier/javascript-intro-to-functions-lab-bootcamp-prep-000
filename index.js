dbd87cd0bd0a6bca612de098484c6575f163c7bd
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-10-14 12:18:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  return string.toUpperCase()
+function logShout(console.log) {
+  return console.log.toUpperCase()
 }
